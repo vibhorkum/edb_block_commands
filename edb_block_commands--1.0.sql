66f7bee99b2ea9cc3a610a392e1b9d3076df7428
vibhorkum <vibhor.aim@gmail.com>
2018-03-16 19:37:21 +0000

    fixing the function names

--- edb_block_commands--1.0.sql
+++ edb_block_commands--1.0.sql
@@ -16,18 +16,18 @@ LANGUAGE C STRICT;
 REVOKE EXECUTE ON FUNCTION edb_switch_user(text) FROM PUBLIC;
 REVOKE EXECUTE ON FUNCTION edb_switch_user(text, text) FROM PUBLIC;
 
-CREATE FUNCTION reedb_switch_user()
+CREATE FUNCTION reset_edb_switch_user()
 RETURNS text
 AS 'MODULE_PATHNAME', 'edb_switch_user'
 LANGUAGE C;
 
-CREATE FUNCTION reedb_switch_user(text)
+CREATE FUNCTION reset_edb_switch_user(text)
 RETURNS text
 AS 'MODULE_PATHNAME', 'edb_switch_user'
 LANGUAGE C STRICT;
 
-GRANT EXECUTE ON FUNCTION reedb_switch_user() TO PUBLIC;
-GRANT EXECUTE ON FUNCTION reedb_switch_user(text) TO PUBLIC;
+GRANT EXECUTE ON FUNCTION reset_edb_switch_user() TO PUBLIC;
+GRANT EXECUTE ON FUNCTION reset_edb_switch_user(text) TO PUBLIC;
 
 /* New functions in 1.1 (now 1.4) begin here */
 
